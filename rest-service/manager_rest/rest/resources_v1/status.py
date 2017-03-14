e7e164a1d5a15c219761ebe1f5ab7088cf674cef
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2017-03-14 12:59:26 +0100

    CFY-6553 Remove the runit/docker part from the status endpoint
    Introduced in CFY-1308, the docker part was obsolete and long not used.

--- rest-service/manager_rest/rest/resources_v1/status.py
+++ rest-service/manager_rest/rest/resources_v1/status.py
@@ -14,8 +14,6 @@
 #  * limitations under the License.
 #
 
-import os
-
 from flask_restful_swagger import swagger
 
 from manager_rest.rest import responses
@@ -44,55 +42,38 @@ class Status(SecuredResource):
         Get the status of running system services
         """
         try:
-            if self._is_docker_env():
-                job_list = {'riemann': 'Riemann',
-                            'rabbitmq-server': 'RabbitMQ',
-                            'celeryd-cloudify-management': 'Celery Management',
-                            'cloudify-ui': 'Cloudify UI',
-                            'logstash': 'Logstash',
-                            'nginx': 'Webserver',
-                            'rest-service': 'Manager Rest-Service',
-                            'amqp-influx': 'AMQP InfluxDB'
-                            }
-                from manager_rest.runitsupervise import get_services
-                jobs = get_services(job_list)
-            else:
-                from manager_rest.systemddbus import get_services
-                job_list = {'cloudify-mgmtworker.service': 'Celery Management',
-                            'cloudify-restservice.service':
-                                'Manager Rest-Service',
-                            'cloudify-amqpinflux.service': 'AMQP InfluxDB',
-                            'cloudify-influxdb.service': 'InfluxDB',
-                            'cloudify-rabbitmq.service': 'RabbitMQ',
-                            'cloudify-riemann.service': 'Riemann',
-                            'cloudify-webui.service': 'Cloudify UI',
-                            'logstash.service': 'Logstash',
-                            'nginx.service': 'Webserver',
-                            'postgresql-9.5.service': 'PostgreSQL'
-                            }
+            from manager_rest.systemddbus import get_services
+            job_list = {'cloudify-mgmtworker.service': 'Celery Management',
+                        'cloudify-restservice.service':
+                            'Manager Rest-Service',
+                        'cloudify-amqpinflux.service': 'AMQP InfluxDB',
+                        'cloudify-influxdb.service': 'InfluxDB',
+                        'cloudify-rabbitmq.service': 'RabbitMQ',
+                        'cloudify-riemann.service': 'Riemann',
+                        'cloudify-webui.service': 'Cloudify UI',
+                        'logstash.service': 'Logstash',
+                        'nginx.service': 'Webserver',
+                        'postgresql-9.5.service': 'PostgreSQL'
+                        }
 
-                if self._is_clustered():
-                    # clustered postgresql service is named differently -
-                    # the old service is not used in a clustered manager,
-                    # so we can ignore its status
-                    del job_list['postgresql-9.5.service']
+            if self._is_clustered():
+                # clustered postgresql service is named differently -
+                # the old service is not used in a clustered manager,
+                # so we can ignore its status
+                del job_list['postgresql-9.5.service']
 
-                    # services that are only running in a clustered manager
-                    job_list.update({
-                        'cloudify-postgresql.service': 'PostgreSQL',
-                        'cloudify-consul.service': 'Consul',
-                        'cloudify-syncthing.service': 'Syncthing',
-                    })
+                # services that are only running in a clustered manager
+                job_list.update({
+                    'cloudify-postgresql.service': 'PostgreSQL',
+                    'cloudify-consul.service': 'Consul',
+                    'cloudify-syncthing.service': 'Syncthing',
+                })
 
-                jobs = get_services(job_list)
+            jobs = get_services(job_list)
         except ImportError:
             jobs = ['undefined']
 
         return dict(status='running', services=jobs)
-
-    @staticmethod
-    def _is_docker_env():
-        return os.getenv('DOCKER_ENV') is not None
 
     @staticmethod
     def _is_clustered():
